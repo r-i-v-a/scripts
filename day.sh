286798772fb7c908c023b984ea58e88163dcc7dd
Riva Nathans <rnathans-cf@dnanexus.com>
2021-07-23 20:31:29 +0200

    Initial commit -- Friday and daily report scripts.

---
+++ day.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+listTasks () {
+	for f in `find . -name "*.txt" -maxdepth 1 -mtime -1`; do
+		head -n 1 $f
+	done
+}
+
+cd ~/Documents/__2__/
+listTasks
+
+cd ~/Documents/__1__/
+listTasks
+
+cd ~/Documents/__h__/
+listTasks
---
+++ friday.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+listTasks () {
+	for f in *.txt; do
+		head -n 1 $f
+	done
+}
+
+cd ~/Documents/__2__/
+printf "\nDone\n"
+listTasks
+
+cd ~/Documents/__1__/
+printf "\nIn Progress\n"
+listTasks
