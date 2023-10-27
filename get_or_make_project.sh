48b5eb4dc73ef7383dd313a20181c9b7503f5956
Riva Kepych <rkepych@dnanexus.com>
2023-10-27 17:23:51 +0200

    Default region eu-central-1

--- get_or_make_project.sh
+++ get_or_make_project.sh
@@ -5,5 +5,5 @@ NAME=$1
 dx select $NAME
 if [[ $? -ne 0 ]]; then
 	echo "Creating new project $NAME"
-	dx new project $NAME --region=aws:us-east-1 --bill-to=org-dnanexus_apps
+	dx new project $NAME --region=aws:eu-central-1 --bill-to=org-dnanexus_apps
 fi
